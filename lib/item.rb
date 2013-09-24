216f8f906dc8b8ab9dbc47df6088ff707b744b43
Jonah Moses <jonahkmoses@gmail.com>
2013-09-23 18:24:14 -0600

    item_test+item.rb & invoice_items_test+invoice_item.rb

---
+++ lib/item.rb
@@ -0,0 +1,21 @@
+class Item
+
+  attr_reader :item_id, 
+              :name, 
+              :description, 
+              :unit_price, 
+              :merchant_id, 
+              :created_at, 
+              :updated_at
+
+  def initialize(item_attributes)
+    @item_id     = item_attributes[:id]
+    @name        = item_attributes[:name]
+    @description = item_attributes[:description]
+    @unit_price  = item_attributes[:unit_price]
+    @merchant_id = item_attributes[:merchant_id]
+    @created_at  = item_attributes[:created_at]
+    @updated_at  = item_attributes[:updated_at]
+  end
+
+end
---
+++ test/item_test.rb
@@ -0,0 +1,58 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require 'csv'
+
+
+class ItemTest < MiniTest::Test 
+
+  def contents
+    CSV.read "./data/items.csv", headers: true, header_converters: :symbol
+  end
+
+  def item_attributes
+    contents.each do |row|
+      id          = row[:id]
+      name        = row[:name]
+      description = row[:description]
+      unit_price  = row[:unit_price]
+      merchant_id = row[:merchant_id]
+      created_at  = row[:created_at]
+      updated_at  = row[:updated_at]
+    end
+  end
+
+  def item
+    item ||= Item.new(item_attributes)
+  end
+
+  def test_item_id
+    assert_equal item_attributes[:id], item.item_id
+  end
+
+  def test_name
+    assert_equal item_attributes[:name], item.name
+  end
+
+  def test_description
+    assert_equal item_attributes[:description], item.description
+  end
+
+  def test_unit_price
+    assert_equal item_attributes[:unit_price], item.unit_price
+  end
+
+  def test_merchant_id
+    assert_equal item_attributes[:merchant_id], item.merchant_id
+  end
+
+  def test_created_at
+    assert_equal item_attributes[:created_at], item.created_at
+  end
+
+  def test_updated_at
+    assert_equal item_attributes[:updated_at], item.updated_at
+  end
+
+end
